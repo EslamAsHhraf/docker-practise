3c554df1bd6ac2e7e6b9d7bfbb8bb4bf7c041a68
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2025-05-11 01:44:46 +0300

    inti the project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require( 'express' );
+
+const app = express();
+const port = 4000;
+
+app.get( '/', ( req, res ) =>
+{
+    res.send( 'Hello World! hi' );
+})
+
+app.listen( port, () => {
+  console.log( `Server is running on http://localhost:${ port }` );
+});
